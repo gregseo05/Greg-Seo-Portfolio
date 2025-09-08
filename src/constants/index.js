372ff6ca4d8034ceeaddb0778c8ac7d80951f75b
jam <jiwpark@umich.edu>
2025-09-07 22:36:01 -0400

    meoeew

--- src/constants/index.js
+++ src/constants/index.js
@@ -14,65 +14,65 @@ const navLinks = [
 ];
 
 const words = [
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
-    { text: "Ideas", imgPath: "/images/ideas.svg" },
-    { text: "Concepts", imgPath: "/images/concepts.svg" },
-    { text: "Designs", imgPath: "/images/designs.svg" },
-    { text: "Code", imgPath: "/images/code.svg" },
+    { text: "Ideas", imgPath: "images/ideas.svg" },
+    { text: "Concepts", imgPath: "images/concepts.svg" },
+    { text: "Designs", imgPath: "images/designs.svg" },
+    { text: "Code", imgPath: "images/code.svg" },
+    { text: "Ideas", imgPath: "images/ideas.svg" },
+    { text: "Concepts", imgPath: "images/concepts.svg" },
+    { text: "Designs", imgPath: "images/designs.svg" },
+    { text: "Code", imgPath: "images/code.svg" },
 ];
 
 const logoIconsList = [
     {
-        imgPath: "/images/logos/company-logo-1.png",
+        imgPath: "images/logos/company-logo-1.png",
     },
     {
-        imgPath: "/images/logos/company-logo-2.png",
+        imgPath: "images/logos/company-logo-2.png",
     },
     {
-        imgPath: "/images/logos/company-logo-3.png",
+        imgPath: "images/logos/company-logo-3.png",
     },
     {
-        imgPath: "/images/logos/company-logo-4.png",
+        imgPath: "images/logos/company-logo-4.png",
     },
     {
-        imgPath: "/images/logos/company-logo-5.png",
+        imgPath: "images/logos/company-logo-5.png",
     },
     {
-        imgPath: "/images/logos/company-logo-6.png",
+        imgPath: "images/logos/company-logo-6.png",
     },
     {
-        imgPath: "/images/logos/company-logo-7.png",
+        imgPath: "images/logos/company-logo-7.png",
     },
     {
-        imgPath: "/images/logos/company-logo-8.png",
+        imgPath: "images/logos/company-logo-8.png",
     },
     {
-        imgPath: "/images/logos/company-logo-9.png",
+        imgPath: "images/logos/company-logo-9.png",
     },
     {
-        imgPath: "/images/logos/company-logo-10.png",
+        imgPath: "images/logos/company-logo-10.png",
     },
     {
-        imgPath: "/images/logos/company-logo-11.png",
+        imgPath: "images/logos/company-logo-11.png",
     },
 ];
 
 const abilities = [
     {
-        imgPath: "/images/seo.png",
+        imgPath: "images/seo.png",
         title: "Analytical Thinking",
         desc: "Ability to break down complex datasets, identify patterns, and draw insights that guide decisions.",
     },
     {
-        imgPath: "/images/chat.png",
+        imgPath: "images/chat.png",
         title: "Data Communication",
         desc: "Skilled at creating clear visualizations and reports that make data easy to understand.",
     },
     {
-        imgPath: "/images/time.png",
+        imgPath: "images/time.png",
         title: "Business Insight",
         desc: "Understands how data connects to business goals, ensuring analysis drives practical impact and value.",
     },
@@ -81,23 +81,23 @@ const abilities = [
 const techStackImgs = [
     {
         name: "React Developer",
-        imgPath: "/images/logos/react.png",
+        imgPath: "images/logos/react.png",
     },
     {
         name: "Python Developer",
-        imgPath: "/images/logos/python.svg",
+        imgPath: "images/logos/python.svg",
     },
     {
         name: "SQL Developer",
-        imgPath: "/images/sql_icon.svg",
+        imgPath: "images/sql_icon.svg",
     },
     {
         name: "Excel Developer",
-        imgPath: "/images/excel_icon.png",
+        imgPath: "images/excel_icon.png",
     },
     {
         name: "Tableau Developer",
-        imgPath: "/images/tableau_icon.png",
+        imgPath: "images/tableau_icon.png",
     },
 ];
 
@@ -137,8 +137,8 @@ const expCards = [
     {
         review:
             "Greg brought strong analytical and customer-focused expertise, improving data accuracy and uncovering insights that directly influenced revenue growth.",
-        /*imgPath: "/images/exp1.png",
-        logoPath: "/images/logo4.jpg",*/
+        /*imgPath: "images/exp1.png",
+        logoPath: "images/logo4.jpg",*/
         title: "Customer Data & Marketing Analyst",
         date: "January 2017 - Jun 2025",
         responsibilities: [
@@ -150,8 +150,8 @@ const expCards = [
     {
         review:
             "Greg combined data-driven insights with strong collaboration skills to enhance store operations and sales performance.",
-        imgPath: "/images/exp2.png",
-        logoPath: "/images/logo2.png",
+        imgPath: "images/exp2.png",
+        logoPath: "images/logo2.png",
         title: "Sales Associate",
         date: "January 2023 - November 2023",
         responsibilities: [
@@ -163,8 +163,8 @@ const expCards = [
     {
         review:
             "Greg delivered excellent customer service in a fast-paced, high-volume restaurant, ensuring accuracy, efficiency, and positive guest experiences.",
-        imgPath: "/images/exp3.png",
-        logoPath: "/images/logo3.png",
+        imgPath: "images/exp3.png",
+        logoPath: "images/logo3.png",
         title: "Server",
         date: "June 2022 - December 2022",
         responsibilities: [
@@ -178,15 +178,15 @@ const expCards = [
 const expLogos = [
     {
         name: "logo1",
-        imgPath: "/images/logo1.png",
+        imgPath: "images/logo1.png",
     },
     {
         name: "logo2",
-        imgPath: "/images/logo2.png",
+        imgPath: "images/logo2.png",
     },
     {
         name: "logo3",
-        imgPath: "/images/logo3.png",
+        imgPath: "images/logo3.png",
     },
 ];
 
@@ -196,42 +196,42 @@ const testimonials = [
         mentions: "@estherhoward",
         review:
             "I can’t say enough good things about Adrian. He was able to take our complex project requirements and turn them into a seamless, functional website. His problem-solving abilities are outstanding.",
-        imgPath: "/images/client1.png",
+        imgPath: "images/client1.png",
     },
     {
         name: "Wade Warren",
         mentions: "@wadewarren",
         review:
             "Working with Adrian was a fantastic experience. He transformed our outdated website into a modern, user-friendly platform. His attention to detail and commitment to quality are unmatched. Highly recommend him for any web dev projects.",
-        imgPath: "/images/client3.png",
+        imgPath: "images/client3.png",
     },
     {
         name: "Guy Hawkins",
         mentions: "@guyhawkins",
         review:
             "Collaborating with Adrian was an absolute pleasure. His professionalism, promptness, and dedication to delivering exceptional results were evident throughout our project. Adrian's enthusiasm for every facet of development truly stands out. If you're seeking to elevate your website and elevate your brand, Adrian is the ideal partner.",
-        imgPath: "/images/client2.png",
+        imgPath: "images/client2.png",
     },
     {
         name: "Marvin McKinney",
         mentions: "@marvinmckinney",
         review:
             "Adrian was a pleasure to work with. He turned our outdated website into a fresh, intuitive platform that’s both modern and easy to navigate. Fantastic work overall.",
-        imgPath: "/images/client5.png",
+        imgPath: "images/client5.png",
     },
     {
         name: "Floyd Miles",
         mentions: "@floydmiles",
         review:
             "Adrian’s expertise in web development is truly impressive. He delivered a robust and scalable solution for our e-commerce site, and our online sales have significantly increased since the launch. He’s a true professional!",
-        imgPath: "/images/client4.png",
+        imgPath: "images/client4.png",
     },
     {
         name: "Albert Flores",
         mentions: "@albertflores",
         review:
             "Adrian was a pleasure to work with. He understood our requirements perfectly and delivered a website that exceeded our expectations. His skills in both frontend and backend dev are top-notch.",
-        imgPath: "/images/client6.png",
+        imgPath: "images/client6.png",
     },
 ];
 
@@ -239,22 +239,22 @@ const socialImgs = [
     {
         name: "insta",
         url: "https://www.instagram.com/",
-        imgPath: "/images/insta.png",
+        imgPath: "images/insta.png",
     },
     {
         name: "fb",
         url: "https://www.facebook.com/",
-        imgPath: "/images/fb.png",
+        imgPath: "images/fb.png",
     },
     {
         name: "x",
         url: "https://www.x.com/",
-        imgPath: "/images/x.png",
+        imgPath: "images/x.png",
     },
     {
         name: "linkedin",
         url: "https://www.linkedin.com/in/gregseo/",
-        imgPath: "/images/linkedin.png",
+        imgPath: "images/linkedin.png",
     },
 ];
 
